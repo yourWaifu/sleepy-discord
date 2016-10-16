0e670d65f398a91a148ec49636aabb1fd3bea84a
NoNamer64 <wuhao64@gmail.com>
2016-10-16 17:25:19 -0400

    fixed the backgourd withjavascript

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-function reply_click(clicked_id)
-{
+function reply_click(clicked_id) {
 	var backgorund = document.getElementById("backgorund");
 	var style = window.getComputedStyle(backgorund);
 	var animationName = style.animationName;
-    backgorund.style.animationName = animationName === "rotate" ? "off" : "rotate";
+	backgorund.style.animationName = animationName === "rotate" ? "off" : "rotate";
+}
+
+window.onload = function() {
+	resize();
+}
+
+function resize() {
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	var screenHypotenuse = Math.sqrt((width * width) + (height * height));
+	var cssWidth = screenHypotenuse / width;
+	var cssHeight = screenHypotenuse / height;
+	var style = document.getElementById("backgorund").style;
+	style.width = (cssWidth * 100) + "%";
+	style.height = (cssHeight * 100) + "%";
+	style.left = -((cssWidth - 1) * 50) + "%";
+	style.top = -((cssHeight - 1) * 50) + "%";
 }
