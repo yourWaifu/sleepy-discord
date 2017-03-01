e2e34875d6c4f037e0ebe543fed6fb4e4574a833
NoNamer64 <wuhao64@gmail.com>
2017-02-28 22:02:39 -0500

    Added some features for the docs and more
    Search is comming soonish I hope
    removed unneeded code for example
    clicking on where it was defined takes you to where it's defined
    fixed some bugs with the main page
    like how rotaton would teleport to 0 deg when paused
    or how the backgound continues forever
    with the last bug fixed, the placeholder isn't needed anymore
    I also made the backgound more slower

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 function reply_click(clicked_id) {
 	var backgorund = document.getElementById("backgorund");
 	var style = window.getComputedStyle(backgorund);
-	var animationName = style.animationName;
-	backgorund.style.animationName = animationName === "rotate" ? "off" : "rotate";
+	var transformMatrix = style.transform;
+	backgorund.style.animationDuration = style.animationDuration === "0s" ? style.transitionDuration : "0s";
+	backgorund.style.transform = transformMatrix;
 }
 
 window.onload = function() {
 	resize();
+	var backgorund = document.getElementById("backgorund");
+	backgorund.style.transitionDuration = window.getComputedStyle(backgorund).animationDuration;	//this is where we store the animationDuration for later
 }
 
 function resize() {
 	var width = window.innerWidth;
 	var height = window.innerHeight;
+	//resize backgorund
 	var screenHypotenuse = Math.sqrt((width * width) + (height * height));
 	var cssWidth = screenHypotenuse / width;
 	var cssHeight = screenHypotenuse / height;
-	var style = document.getElementById("backgorund").style;
-	style.width = (cssWidth * 100) + "%";
-	style.height = (cssHeight * 100) + "%";
-	style.left = -((cssWidth - 1) * 50) + "%";
-	style.top = -((cssHeight - 1) * 50) + "%";
+	var backgorundStyle = document.getElementById("backgorund").style;
+	backgorundStyle.width = (cssWidth * 100) + "%";
+	backgorundStyle.height = (cssHeight * 100) + "%";
+	backgorundStyle.left = -((cssWidth - 1) * 50) + "%";
+	backgorundStyle.top = -((cssHeight - 1) * 50) + "%";
+	//resize main content
+	var mainSectionRect = document.getElementById("mainSection").getBoundingClientRect();
+	if (mainSectionRect.bottom < height) {
+		var mainSectionStyle = document.getElementById("mainSection").style;
+		mainSectionStyle.height = (height - mainSectionRect.top) + "px"
+	}
 }
