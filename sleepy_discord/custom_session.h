90ffcf971905a288b1644cf710546c2673e99780
NoNamer64 <wuhao64@gmail.com>
2017-05-20 14:53:24 -0400

    added CustomSession

--- sleepy_discord/custom_session.h
+++ sleepy_discord/custom_session.h
@@ -1,12 +1,29 @@
 #pragma once
+#include <memory>
 #include "http.h"
 
 namespace SleepyDiscord {
+	typedef GenericSession* (*const CustomInit)();
+
 	class CustomSession : public GenericSession {
 	public:
-		
+		static CustomInit init;
+		CustomSession() : session(init()) {}
+		inline void setUrl(const std::string& url) { session->setUrl(url); }
+		inline void setBody(const std::string* jsonParamters) { session->setBody(jsonParamters); }
+		inline void setHeader(const std::initializer_list<std::pair<std::string, std::string>>& header) {
+			session->setHeader(header);
+		}
+		inline void setMultipart(const std::initializer_list<Part>& parts) {
+			session->setMultipart(parts);
+		}
+		inline Response Post  () { return session->Post  (); }
+		inline Response Patch () { return session->Patch (); }
+		inline Response Delete() { return session->Delete(); }
+		inline Response Get   () { return session->Get   (); }
+		inline Response Put   () { return session->Put   (); }
 	private:
-		void * session; //pointer to the customSession
+		std::unique_ptr<GenericSession> session; //pointer to session
 	};
 
 	typedef CustomSession Session;
