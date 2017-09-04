f9e29370e5fefc83d17cb90aba748fc5af01d4e9
NoNamer64 <wuhao64@gmail.com>
2017-09-04 18:50:05 -0400

    fixed some formating issues, I think

--- sleepy_discord/webhook.cpp
+++ sleepy_discord/webhook.cpp
@@ -4,11 +4,11 @@ SleepyDiscord::Webhook::Webhook(const std::string * rawJSON) {
 	std::vector<std::string> values = json::getValues(rawJSON->c_str(),
 	{ "id", "guild_id", "channel_id", "user", "name", "avatar", "token" });
 	unsigned int i = -1;
-	id		   = values[i++];
+	id         = values[i++];
 	server_id  = isDefined(values[i++]) ? values[i] : "";
 	channel_id = values[i++];
-	user	   = User(&values[i++]);
-	name	   = isDefined(values[i++]) ? values[i] : "";
-	avatar	   = isDefined(values[i++]) ? values[i] : "";
-	token	   = values[i++];
+	user       = User(&values[i++]);
+	name       = isDefined(values[i++]) ? values[i] : "";
+	avatar     = isDefined(values[i++]) ? values[i] : "";
+	token      = values[i++];
 }
