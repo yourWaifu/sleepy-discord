c9a4ac13e9dc1f55e6e1eceb7adaab7eec44bb53
NoNamer64 <wuhao64@gmail.com>
2017-09-04 18:44:24 -0400

    fixed issues with json parser
    json used to now work with spaces, now it does
    you can now convert numbers using some new functions
    Also it's now can be compiled using c++11

--- sleepy_discord/discord_object_interface.h
+++ sleepy_discord/discord_object_interface.h
@@ -29,19 +29,38 @@ namespace SleepyDiscord {
 		inline bool isDefault(const std::string& string) {	
 			return isSpecified(string) || string == "null";
 		}
+
+		template<typename Number, typename Function>
+		inline void implementToNumber(std::string& value, Number& number, Function convertFunction) {
+			number = isDefined(value) ? convertFunction(value) : 0;
+		}
+
+		inline void toNumber(int& number, std::string& value) {
+			implementToNumber(value, number, [](std::string& v)->int {
+				return std::stoi(v);
+			});
+		}
+
+		inline void toNumber(unsigned long& number, std::string& value) {
+			implementToNumber(value, number, [](std::string& v)->unsigned long {
+				return std::stoul(v);
+			});
+		}
 	};
 
+	//constexpr unsigned int index(std::initializer_list<const char *const> names, const char * name, unsigned int i = 0) {
+	//	for (const char *const n : names)
+	//		if (strcmp(n, name) != 0) ++i;
+	//		else break;
+	//	return i;
+	//}//sadly this doesn't work on c++11
+
 	constexpr unsigned int index(std::initializer_list<const char *const> names, const char * name, unsigned int i = 0) {
-		for (const char *const n : names)
-			if (strcmp(n, name) != 0) ++i;
-			else break;
-			return i;
+		return i + names.begin() != names.end() && strcmp(*(i + names.begin()), name) != 0 ? index(names, name, i + 1) : i;
 	}
-
-
 
 	template <class _DiscordObject>
 	std::vector<_DiscordObject> JSON_getArray(const std::string* _source) {
-		return json::getArray<_DiscordObject>(_source, [](_DiscordObject* value, std::string string) {*value = _DiscordObject(&string); });
+		return json::RawJSONArrayWrapper<_DiscordObject>(*_source);
 	}
 }
