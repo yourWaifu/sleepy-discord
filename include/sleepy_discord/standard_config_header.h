496a5eeef577dfe6c7637944cf9d3d3f5cb5d457
NoNamer64 <wuhao64@gmail.com>
2018-01-02 16:12:09 -0500

    You can now choose which standard functions are included

--- include/sleepy_discord/standard_config_header.h
+++ include/sleepy_discord/standard_config_header.h
@@ -1,3 +1,8 @@
 #pragma once
+#ifndef SLEEPY_DO_NOT_INCLUDE_STANDARD_ONERROR
 virtual void onError(SleepyDiscord::ErrorCode errorCode, const std::string errorMessage);
+#endif
+
+#ifndef SLEEPY_DO_NOT_INCLUDE_STANDARD_SLEEP
 virtual void sleep(const unsigned int milliseconds);
+#endif
