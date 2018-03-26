e5636ea0b0112cd4701845182aaf3e9913a3d6fd
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2018-03-25 22:09:58 -0300

    Fixed the setup steps for CPR on linux

--- setup.py
+++ setup.py
@@ -82,6 +82,8 @@ def installCPR():
 		run("deps\\cpr\\opt\\curl\\buildconf.bat")
 		#run("C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\vcvars32.bat")
 	if linux:
+		os.chmod("./deps/cpr/opt/curl/buildconf", 0o777)
+		os.chdir("./deps/cpr/opt/curl")
 		run("./deps/cpr/opt/curl/buildconf")
 
 def installWebsocketPP():
