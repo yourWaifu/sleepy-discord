19821ee8e76c87bcc2600ef9aeb3891fe26b1d09
Sleepy Flower Girl <wuhao64@gmail.com>
2018-04-22 07:37:01 -0400

    Voice is now supported

---
+++ include/sleepy_discord/voice_connection.cpp
@@ -0,0 +1,311 @@
+#ifdef SLEEPY_VOICE_ENABLED
+#include <sodium.h>
+#include "voice_connection.h"
+#include "client.h"
+
+namespace SleepyDiscord {
+	void VoiceConnection::disconnect()  {
+		if (state & State::CONNECTED)
+			origin->disconnect(1000, "", &connection);
+		if (heart.isValid())
+			heart.stop(); //Kill
+		state = static_cast<State>(state & ~State::CONNECTED);
+	}
+
+	void VoiceConnection::processMessage(std::string message) {
+
+		origin->onError(ERROR_NOTE, "testing VOICE processMessage");
+
+		//to do there is reused code here, plase place them into functions
+		std::vector<std::string> values = json::getValues(message.c_str(),
+			{ "op", "d" });
+		VoiceOPCode op = static_cast<VoiceOPCode>(std::stoi(values[0]));
+		std::string *d = &values[1];
+		switch (op) {
+		case HELLO: {
+			origin->onError(ERROR_NOTE, "testing VOICE HELLO");
+
+			heartbeatInterval = std::stoi(json::getValue(d->c_str(), "heartbeat_interval"));
+			std::string identity;
+			/*The number 116 comes from the number of letters in this string + 1:
+				{"op": 0,"d": {"server_id": "18446744073709551615",
+				"user_id": "18446744073709551615","session_id": "","token": ""}}
+			*/
+			//remember to change the number below when editing identity
+			identity.reserve(116 + context.sessionID.length() + origin->token->length());
+			identity +=
+				"{"
+					"\"op\": 0," //VoiceOPCode::IDENTIFY
+					"\"d\": {"
+						"\"server_id\": \"" ; identity += context.serverID ; identity += "\","
+						"\"user_id\": \""   ; identity += origin->getID()  ; identity += "\","
+						"\"session_id\": \""; identity += context.sessionID; identity += "\","
+						"\"token\": \""     ; identity += context.token    ; identity += "\""
+					"}"
+				"}";
+			origin->send(identity, &connection);
+			}
+			state = static_cast<State>(state | CONNECTED);
+			break;
+		case READY: {
+			origin->onError(ERROR_NOTE, "testing VOICE READY");
+
+			std::vector<std::string> values = json::getValues(d->c_str(),
+			{ "ssrc", "port" });
+			sSRC = std::stoul(values[0]);
+			port = static_cast<uint16_t>(std::stoul(values[1]));
+			//start heartbeating
+			heartbeat();
+			//connect to UDP
+			//IP Discovery
+			unsigned char packet[70] = { 0 };
+			packet[0] = (sSRC >> 24) & 0xff;
+			packet[1] = (sSRC >> 16) & 0xff;
+			packet[2] = (sSRC >>  8) & 0xff;
+			packet[3] = (sSRC      ) & 0xff;
+			UDPClient::send(context.endpoint, port, packet, 70);
+			const std::vector<uint8_t> iPDiscovery = UDPClient::receive(context.endpoint, port); //note: receive blocks and is not async
+			//find start of string. 0x60 is a bitmask that should filter out non-letters
+			std::vector<uint8_t>::const_iterator iPStart = iPDiscovery.begin() + 2;
+			const std::string iPAddress(iPStart, std::find(iPStart, iPDiscovery.end(), 0)); //find may not be needed
+			//send Select Protocol Payload
+			std::string protocol;
+			/*The number 101 comes from the number of letters in this string + 1:
+				{"op": 1,"d": {"protocol": "udp","data": {
+				"address": "","port": 65535,
+				"mode": "xsalsa20_poly1305"}}}
+			*/
+			protocol.reserve(101 + iPAddress.length());
+			protocol +=
+				"{"
+					"\"op\": 1," //VoiceOPCode::SELECT_PROTOCOL
+					"\"d\": {"
+						"\"protocol\": \"udp\","
+						"\"data\": {"
+							"\"address\": \""; protocol += iPAddress           ; protocol += "\","
+							"\"port\": "     ; protocol += std::to_string(port); protocol +=   ","
+							"\"mode\": \"xsalsa20_poly1305\""
+						"}"
+					"}"
+				"}";
+			origin->send(protocol, &connection);
+			}
+			state = static_cast<State>(state | State::OPEN);
+			break;
+		case SESSION_DESCRIPTION:
+			origin->onError(ERROR_NOTE, "testing VOICE SESSION_DESCRIPTION");
+
+			{
+			std::vector<std::string> stringArray = json::getArray(&json::getValue(d->c_str(), "secret_key"));
+			const unsigned int stringArraySize = stringArray.size();
+			for (unsigned int i = 0; i < SECRET_KEY_SIZE && i < stringArraySize; ++i) {
+				if (stringArray[i] != "")
+					secretKey[i] = std::stoul(stringArray[i]) & 0xFF;
+			}
+			}
+			state = static_cast<State>(state | State::AUDIO_ENABLED);
+			if (context.eventHandler != nullptr)
+				context.eventHandler->onReady(*this);
+			break;
+		case RESUMED:
+			origin->onError(ERROR_NOTE, "testing VOICE RESUMED");
+			break;
+		case HEARTBEAT_ACK:
+			origin->onError(ERROR_NOTE, "testing VOICE HEARTBEAT_ACK");
+			break;
+		default:
+			origin->onError(ERROR_NOTE, "testing VOICE default");
+			break;
+		}
+	}
+
+	void VoiceConnection::heartbeat() {
+
+		origin->onError(ERROR_NOTE, "testing VOICE heartbeat");
+
+		//timestamp int
+		const uint64_t bitMask52 = 0x1FFFFFFFFFFFFF;
+		const uint64_t currentTime = static_cast<uint16_t>(origin->getEpochTimeMillisecond());
+		const std::string nonce = std::to_string(bitMask52 & currentTime);
+		/*The number 17 comes from the number of letters in this string + 1:
+		{"op": 3, "d": }
+		*/
+		std::string heartbeat;
+		heartbeat.reserve(17 + nonce.length());
+		heartbeat += 
+			"{"
+				"\"op\": 3, "
+				"\"d\": "; heartbeat += nonce; heartbeat += 
+			'}';
+		origin->send(heartbeat, &connection);
+
+		heart = origin->schedule([this]() {
+			this->heartbeat();
+		}, heartbeatInterval);
+	}
+
+	void VoiceConnection::startSpeaking() {
+		if ((state & State::ABLE) != State::ABLE) return;
+
+		//init libraries if havn't and not encoded data
+		//init sodium
+		if (sodium_init() < 0)
+			return;
+
+		if (!audioSource->isOpusEncoded())
+#if defined(NONEXISTENT_OPUS)
+			return;
+#else
+			if (!(state & CAN_ENCODE)) {
+				//init opus
+				int opusError = 0;
+				encoder = opus_encoder_create(
+					/*Sampling rate(Hz)*/48000,
+					/*Channels*/         2,
+					/*Mode*/             OPUS_APPLICATION_VOIP,
+					&opusError);
+				if (opusError) {//error check
+					return;
+				}
+				//opusError = opus_encoder_ctl(encoder, OPUS_SET_BITRATE(BITRATE));
+				if (opusError) {
+					return;
+				}
+				state = static_cast<State>(state | State::CAN_ENCODE);
+			}
+#endif
+
+		//say something
+		sendSpeaking(true);
+		speak();
+	}
+
+	void VoiceConnection::sendSpeaking(bool isNowSpeaking) {
+		std::string ssrc = std::to_string(sSRC);
+		/*The number 49 comes from 1 plus the length of this string
+			{"op":5,"d":{"speaking":false,"delay":0,"ssrc":}}
+		*/
+		std::string speaking;
+		speaking.reserve(49 + ssrc.length());
+		speaking +=
+			"{"
+				"\"op\":5,"
+				"\"d\":{"
+					"\"speaking\":"; speaking += json::boolean(isNowSpeaking); speaking += ","
+					"\"delay\":0,"
+					"\"ssrc\":"; speaking += ssrc; speaking +=
+				"}"
+			"}";
+		origin->send(speaking, &connection);
+	}
+
+	void VoiceConnection::speak() {
+		//check that we are can still send audio data
+		if ((state & State::ABLE) != State::ABLE)
+			return;
+
+		AudioTransmissionDetails details(
+			context, 
+			static_cast<std::size_t>(48000 * 2 * .02),
+			samplesSentLastTime
+		);
+
+		//send the audio data
+		switch (audioSource->type) {
+		case AUDIO_VECTOR:
+		case AUDIO_VECTOR_U16: {
+			auto audioVectorSource = &static_cast<AudioSource<AUDIO_VECTOR>&>(*audioSource);
+			std::vector<int16_t> audioData = audioVectorSource->read(details);
+			int16_t* audioBuffer = audioData.data();
+			speak(audioBuffer, audioData.size());
+		} break;
+		case AUDIO_POINTER:
+		case AUDIO_POINTER_U16: {
+			auto audioVectorSource = static_cast<AudioSource<AUDIO_POINTER>&>(*audioSource);
+			int16_t * audioData = nullptr;
+			std::size_t length;
+			audioVectorSource.read(details, audioData, length);
+			speak(audioData, length);
+		} break;
+		default:
+			break;
+		}
+	}
+
+	void VoiceConnection::speak(int16_t*& audioData, const std::size_t & length)  {
+		//This is only called in speak() so already checked that we can still send audio data
+
+		//stop sending data when there's no data
+		if (length == 0) {
+			sendSpeaking(false);
+			return;
+		}
+
+		//the >>1 cuts it in half since you are using 2 channels
+		const std::size_t frameSize = length >> 1;
+
+		if (!audioSource->isOpusEncoded()) {
+#if defined(NONEXISTENT_OPUS)
+			return;
+#else
+			//encode data
+			constexpr opus_int32 encodedAudioMaxLength = 960;
+			unsigned char encodedAudioData[encodedAudioMaxLength];
+			opus_int32 encodedAudioLength = opus_encode(
+				encoder, audioData, frameSize,
+				encodedAudioData, encodedAudioMaxLength);
+			//send it
+			uint8_t * encodedAudioDataPointer = encodedAudioData;
+			sendAudioData(encodedAudioDataPointer, encodedAudioLength, frameSize);
+#endif
+		} else {
+			//encoded data should be in uint8
+			sendAudioData(reinterpret_cast<uint8_t*&>(audioData), length, frameSize);
+		}
+
+		//schedule next send
+		origin->schedule([this]() {
+			this->speak();
+		}, 20); //I think 20 is the frame length
+	}
+
+	void VoiceConnection::sendAudioData(
+		uint8_t*& encodedAudioData,
+		const std::size_t & length,
+		const std::size_t & frameSize
+	) {
+		++sequence;
+		constexpr int headerSize = 12;
+
+		const uint8_t header[headerSize] = {
+			0x80,
+			0x78,
+			static_cast<uint8_t>((sequence  >> (8 * 1)) & 0xff),
+			static_cast<uint8_t>((sequence  >> (8 * 0)) & 0xff),
+			static_cast<uint8_t>((timestamp >> (8 * 3)) & 0xff),
+			static_cast<uint8_t>((timestamp >> (8 * 2)) & 0xff),
+			static_cast<uint8_t>((timestamp >> (8 * 1)) & 0xff),
+			static_cast<uint8_t>((timestamp >> (8 * 0)) & 0xff),
+			static_cast<uint8_t>((sSRC      >> (8 * 3)) & 0xff),
+			static_cast<uint8_t>((sSRC      >> (8 * 2)) & 0xff),
+			static_cast<uint8_t>((sSRC      >> (8 * 1)) & 0xff),
+			static_cast<uint8_t>((sSRC      >> (8 * 0)) & 0xff),
+		};
+			
+		uint8_t nonce[24];
+		std::memcpy(nonce                , header, sizeof nonce - sizeof header);
+		std::memset(nonce + sizeof header,      0, sizeof nonce - sizeof header);
+		
+		const size_t numOfBtyes = sizeof header + length + crypto_secretbox_MACBYTES;
+		std::vector<uint8_t> audioDataPacket(numOfBtyes);
+		std::memcpy(audioDataPacket.data(), header, sizeof header);
+
+		crypto_secretbox_easy(audioDataPacket.data() + sizeof header,
+			encodedAudioData, length, nonce, secretKey);
+
+		UDPClient::send(context.endpoint, port, audioDataPacket.data(), audioDataPacket.size());
+		samplesSentLastTime = frameSize << 1;
+		timestamp += frameSize;
+	}
+}
+#endif
---
+++ include/sleepy_discord/voice_connection.h
@@ -0,0 +1,226 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+#include <list>
+#include <opus.h>
+#include "udp_client.h"
+#include "snowflake.h"
+#include "server.h"
+#include "channel.h"
+#include "message_receiver.h"
+#include "timer.h"
+
+namespace SleepyDiscord {
+	class BaseDiscordClient;
+	class VoiceConnection;
+
+	class BaseVoiceEventHandler {
+	public:
+		//to do add more events
+		virtual void onReady(VoiceConnection&) {}
+	};
+
+	struct VoiceContext {
+		friend VoiceConnection;
+		friend BaseDiscordClient;
+	public:
+		inline Snowflake<Channel> getChannelID() {
+			return channelID;
+		}
+		inline Snowflake<Server> getServerID() {
+			return serverID;
+		}
+
+		inline bool operator==(const VoiceContext& right) {
+			return this == &right;
+		}
+
+		inline void setVoiceHandler(BaseVoiceEventHandler& handler) {
+			eventHandler = &handler;
+		}
+
+	private:
+		VoiceContext(Snowflake<Channel> _channelID, Snowflake<Server> _serverID) :
+			channelID(_channelID), serverID(_serverID), eventHandler(nullptr)
+		{}
+
+		Snowflake<Channel> channelID;
+		Snowflake<Server> serverID;
+		std::string sessionID = "";
+		std::string endpoint = "";
+		std::string token; //the same token used to log in?
+		BaseVoiceEventHandler* eventHandler;
+	};
+
+	enum AudioSourceType {
+		AUDIO_BASE_TYPE,
+		AUDIO_VECTOR,
+		AUDIO_VECTOR_U16,
+		AUDIO_POINTER,
+		AUDIO_POINTER_U16
+	};
+
+	struct BaseAudioSource {
+		BaseAudioSource() : type(AUDIO_BASE_TYPE) {}
+		BaseAudioSource(AudioSourceType typ) : type(typ) {}
+		virtual inline bool isOpusEncoded() { return false; }
+		const AudioSourceType type;
+		virtual ~BaseAudioSource() {}
+	};
+
+	//I'm not sure if it's really a good idea for this to be a nested class
+	class VoiceConnection : public UDPClient, public GenericMessageReceiver {
+	public:
+		VoiceConnection(BaseDiscordClient* client, VoiceContext& _context) :
+			origin(client), context(_context) {
+		}
+
+		VoiceConnection(VoiceConnection&&) = default;
+
+		~VoiceConnection() {
+		}
+
+		inline const bool isReady() {
+			return state & State::ABLE;
+		}
+
+		void disconnect();
+
+		template<class AudioSource, class... Types>
+		void startSpeaking(Types&&... arguments) {
+			audioSource = std::unique_ptr<AudioSource>(new AudioSource(std::forward<Types>(arguments)...));
+			startSpeaking();
+		}
+
+		template<class AudioSource>
+		void startSpeaking(AudioSource* source) {
+			audioSource = std::unique_ptr<AudioSource>(source);
+			startSpeaking();
+		}
+
+#ifndef NONEXISTENT_OPUS
+		//bool enableEncoding() {
+		//	//init opus
+		//	encoder = opus_encoder_create(
+		//		/*Sampling rate(Hz)*/48000,
+		//		/*Channels*/         2,
+		//		/*Mode*/             OPUS_APPLICATION_VOIP,
+		//		&opusError);
+		//	if (opusError) {//error check
+		//		return;
+		//	}
+		//	//opusError = opus_encoder_ctl(encoder, OPUS_SET_BITRATE(BITRATE));
+		//	//if (opusError) {
+		//	//	int a;
+		//	//}
+
+		//	//init sodium
+		//	if (sodium_init() < 0)
+		//		return;
+		//}
+#endif // !NONEXISTENT_OPUS
+
+	private:
+		friend BaseDiscordClient;
+
+		void processMessage(std::string message);
+
+		enum VoiceOPCode {
+			IDENTIFY            = 0,  //client begin a voice websocket connection
+			SELECT_PROTOCOL     = 1,  //client select the voice protocol
+			READY               = 2,  //server complete the websocket handshake
+			HEARTBEAT           = 3,  //client keep the websocket connection alive
+			SESSION_DESCRIPTION = 4,  //server describe the session
+			SPEAKING            = 5,  //both   indicate which users are speaking
+			HEARTBEAT_ACK       = 6,  //server sent immediately following a received client heartbeat
+			RESUME              = 7,  //client resume a connection
+			HELLO               = 8,  //server the continuous interval in milliseconds after which the client should send a heartbeat
+			RESUMED             = 9,  //server acknowledge Resume
+			CLIENT_DISCONNECT   = 13  //server a client has disconnected from the voice channel
+		};
+			
+		enum State : uint8_t {
+			NOT_CONNECTED = 0 << 0,
+			CONNECTED     = 1 << 0,
+			OPEN          = 1 << 1,
+			AUDIO_ENABLED = 1 << 2,
+
+			CAN_ENCODE    = 1 << 6,
+			CAN_DECODE    = 1 << 7,
+
+			ABLE          = CONNECTED | OPEN | AUDIO_ENABLED,
+		};
+
+		int heartbeatInterval = 0;
+		uint32_t sSRC;
+		uint16_t port;
+		BaseDiscordClient* origin;
+		Timer heart;
+		State state = State::NOT_CONNECTED;
+		int16_t numOfPacketsSent = 0;
+		std::unique_ptr<BaseAudioSource> audioSource;
+		std::size_t samplesSentLastTime = 0;
+		VoiceContext& context;
+#if !defined(NONEXISTENT_OPUS)
+		OpusEncoder *encoder;
+#endif
+		uint16_t sequence = 0;
+		uint32_t timestamp = 0;
+
+		#define SECRET_KEY_SIZE 32
+		unsigned char secretKey[SECRET_KEY_SIZE];
+
+		void heartbeat();
+		void startSpeaking();
+		void sendSpeaking(bool isNowSpeaking);
+		void speak();
+		void speak(int16_t*& audioData, const std::size_t& length);
+		void sendAudioData(
+			uint8_t*& encodedAudioData,
+			const std::size_t & length,
+			const std::size_t & frameSize
+		);
+	};
+
+	struct AudioTransmissionDetails {
+	public:
+		inline VoiceContext& context() {
+			return _context;
+		}
+
+		inline std::size_t proposedLength() {
+			return _proposedLength;
+		}
+
+		inline std::size_t amountSentSinceLastTime() {
+			return _amountSentSinceLastTime;
+		}
+	private:
+		friend VoiceConnection;
+		AudioTransmissionDetails(VoiceContext& con, const std::size_t pro, const std::size_t amo) :
+			_context(con), _proposedLength(pro), _amountSentSinceLastTime(amo) { }
+
+		VoiceContext& _context;
+		const std::size_t _proposedLength;
+		const std::size_t _amountSentSinceLastTime;
+	};
+
+	template<AudioSourceType Type>
+	struct AudioSource : public BaseAudioSource {
+		const AudioSourceType type = Type;
+	};
+
+	template<>
+	struct AudioSource<AUDIO_VECTOR> : public BaseAudioSource {
+		AudioSource() : BaseAudioSource(AUDIO_VECTOR_U16) {}
+		virtual std::vector<int16_t> read(AudioTransmissionDetails& details) {
+			return std::vector<int16_t>();
+		};
+	};
+
+	template<>
+	struct AudioSource<AUDIO_POINTER> : public BaseAudioSource {
+		AudioSource() : BaseAudioSource(AUDIO_POINTER_U16) {}
+		virtual void read(AudioTransmissionDetails& details, int16_t*& buffer, std::size_t& length) {};
+	};
+}
