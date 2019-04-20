e471143881651a0904409557bb64f6d5e165d337
Sleepy Flower Girl <wuhao64@gmail.com>
2019-04-20 12:36:38 -0400

    Fix build issues in linux
    copied from the develop branch. might fix #156

--- sleepy_discord/experimental.cpp
+++
@@ -1,8 +0,0 @@
-#include "experimental.h"
-#include "client.h"
-
-//std::chrono::high_resolution_clock::time_point startTimer = std::chrono::high_resolution_clock::now();	//this is so that I can collect data from my experiments
-
-namespace SleepyDiscord {
-
-}
