f0a906b1621e28c00b7d5980f2ddb48c566cef9d
Sleepy Flower Girl <wuhao64@gmail.com>
2019-07-29 02:49:15 -0400

    Fixed Travis Ci not liking my travis.yml

--- examples/hello/example0.cpp
+++ examples/hello/example0.cpp
@@ -6,10 +6,12 @@ public:
 	void onMessage(SleepyDiscord::Message message) override {
 		if (message.startsWith("whcg hello"))
 			sendMessage(message.channelID, "Hello " + message.author.username);
+		else if (message.startsWith("whcg test"))
+			testFunction("");
 	}
 };
 
 int main() {
-	MyClientClass client("token", SleepyDiscord::USER_CONTROLED_THREADS);
+	MyClientClass client("NDczMzM5MjY2NTIxNjI4Njcz.XRu71Q.Fw1-3MvIeMZA49nzXX_d7sVaa14", SleepyDiscord::USER_CONTROLED_THREADS);
 	client.run();
 }
