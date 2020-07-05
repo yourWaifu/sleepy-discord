eef638af1fb7c85613791da8041fb49c3882005f
Sleepy Flower Girl <wuhao64@gmail.com>
2020-07-05 16:31:22 -0400

    fix bug where request mode changes Sync or Async

--- sleepy_discord/client.cpp
+++ sleepy_discord/client.cpp
@@ -63,7 +63,7 @@ namespace SleepyDiscord {
 	}
 
 	Response BaseDiscordClient::request(const RequestMethod method, Route path, const std::string jsonParameters,
-		const std::vector<Part>& multipartParameters, RequestCallback callback, RequestMode mode
+		const std::vector<Part>& multipartParameters, RequestCallback callback, const RequestMode mode
 	) {
 		//check if rate limited
 		Response response;
