8a1adf70b7a981a296d7c69cc095e465e054d744
Sleepy Flower Girl <wuhao64@gmail.com>
2021-02-20 19:57:45 -0500

    fix some compiler errors

--- include/sleepy_discord/generic_compression.h
+++ include/sleepy_discord/generic_compression.h
@@ -3,6 +3,7 @@
 namespace SleepyDiscord {
 	class GenericCompression {
 	public:
+		virtual ~GenericCompression() = 0;
 		virtual bool uncompress(const std::string& compressed, std::string& uncompressedOut) = 0;
 	};
 }
